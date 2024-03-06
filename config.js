59e2c6ad300420492355cdb12326be35078b6b4d
Mʀ Iꜱᴜʀᴜ X <156693573+MrIsuru@users.noreply.github.com>
2024-03-06 19:47:45 +0530

    ᴄʜᴀᴍɪᴋᴀ-ᴍᴅ ᴡᴀ ᴜꜱᴇʀ ʙᴏᴛ 🤠

---
+++ config.js
@@ -0,0 +1,114 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+
+//
+global.available = true;
+global.autoReadAll = true;
+global.antitags = true;
+
+
+//auto functioner
+global.autoTyping = false;                //auto tying by default off.
+global.autoRecord = false;                //auto recording by default off.
+global.groupevent = false;                //This is the new variable for controlling group event handling.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
+global.autoreadgc = true;
+
+
+//
+global.prefa = ["."];                                    //Default prefix here. you can change if you want.
+
+
+//
+global.Owner = ["94766632281", "94713330814"];         //If you want singal number so global.Owner = ['94766632281'] Change into your number.
+global.OwnerNumber = ["94766632281", "94713330814"];   //If you want singal number so global.Owner = ['94766632281'] Change into your number.
+global.ownertag = ["94766632281"];
+global.OwnerName = "Mʀ Iꜱᴜʀᴜ X";
+global.BotName = "CHAMIKA-MD";
+global.packname = "ᴄʜᴀᴍɪᴋᴀ-ᴍᴅ 🤠";                             //Do not change.
+global.author = "ᴇᴢʀ - ᴍͥ ʀ  ɪ ꜱͣ ᴜͫ ʀ ᴜ  x";                               //Do not change.
+global.BotSourceCode = "https://github.com/MrIsuru/CHAMIKA-MD_BOT"; //Do not change.
+global.SupportGroupLink = "https://chat.whatsapp.com/G3aoO7etNzeL27hGVDJktH";
+
+
+//
+global.sessionName = "session";                          //Do not change.
+
+
+//
+global.openAiAPI = "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";
+
+
+//
+global.location = "Colombo, Srilanka";
+global.reactmoji = "💀";
+global.themeemoji = "🔮";
+global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
+global.websitex = "https://github.com/MrIsuru";
+global.lolhuman = "KaysaS";
+
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+
+
+//
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+//
+global.mess = {
+  jobdone: '*々 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʏʙᴇʀᴋɪʟʟᴇʀꜱᴛᴇᴀᴍ*',
+  useradmin: '_Sorry, only *Gʀᴏᴜᴘ Aᴅᴍɪɴꜱ* can use this command!_',
+  botadmin: '_Sorry, i cant execute this command without being an *Aᴅᴍɪɴ* of this group!_',
+  botowner: '_Only my *Oᴡɴᴇʀ* can use this command!_',
+  grouponly: '_This command is only made for *Gʀᴏᴜᴘꜱ*!_',
+  privateonly: '_This command is only made for *Pʀɪᴠᴀᴛᴇ Cʜᴀᴛ*!_',
+  botonly: '_Only the *Bᴏᴛ IᴛSᴇʟꜰ* can use this command!_',
+  waiting: '_Just Wait..._',
+  nolink: '_Please provide me *lɪɴᴋ*!_',
+  error: '_An error occurd!_',
+  banned: '_You are *Bᴀɴɴᴇᴅ* fron using commands!_',
+  bangc: '_This Group is *Bᴀɴɴᴇᴅ* from using Commands!_',
+  nonsfw: '_Dont be a pervert! This is not a *NSFW* enabled group!_'
+
+}
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
